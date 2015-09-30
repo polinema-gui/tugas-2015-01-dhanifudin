16b2377d4fc4df3141d85d6c6248c9132773bab9
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2015-09-30 13:30:02 +0700

    implementasi pengubahan property backcolor

---
+++ Project/Project/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Blue Then
+            Me.BackColor = Color.Red
+        Else
+            Me.BackColor = Color.Blue
+        End If
+    End Sub
+End Class
